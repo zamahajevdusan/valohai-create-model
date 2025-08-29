9d5afa210b2b6bb7b040faca4fec9eb71f29c137
Dusan Zamahajev <zamahajev.dusan@gmail.com>
2025-08-29 15:16:16 +0200

    initial pipeline demo

---
+++ create_model_version.py
@@ -0,0 +1,23 @@
+import valohai
+import json
+import shutil
+
+model_url = valohai.parameters("model_url")
+
+metadata = {
+    "model.h5": {
+        "factory": "eu-02",
+        "valohai.tags": ["prod", "lemonade"],
+        "valohai.model-versions": [model_url]
+    },
+}
+
+# save all the models separately;
+save_path = "/valohai/outputs/model.h5"
+shutil.move("/valohai/inputs/model/model.h5", save_path)
+
+metadata_path = "/valohai/outputs/valohai.metadata.jsonl"
+with open(metadata_path, "w") as outfile:
+    for file_name, file_metadata in metadata.items():
+        json.dump({"file": file_name, "metadata": file_metadata}, outfile)
+        outfile.write("\n")
